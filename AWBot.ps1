164cd5d38886cd9997f5ec55a61d87ee4eb241ee
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-10-01 15:39:10 +0530

    Add files via upload

--- AWBot.ps1
+++ AWBot.ps1
@@ -1,8 +1,9 @@
+Expand-Archive -Path "C:\Users\Administrator\Downloads\Awesome Traffic Bot-bing.zip" -DestinationPath "C:\Program Files (x86)\DalgaDev\Awesome Traffic Bot" -Force
+Write-Host "Replaced all file"
 $exeFilePath = "C:\Users\Administrator\Downloads\ATB-UPDATE-60-175-9nj.zip\ATB Installer.exe"
 Start-Process -FilePath $exeFilePath -Wait
 Write-Host "Installation complete."
-Expand-Archive -Path "C:\Users\Administrator\Downloads\Awesome-Traffic-Bot-SE-Patch2.zip" -DestinationPath "C:\Program Files (x86)\DalgaDev\Awesome Traffic Bot" -Force
-Write-Host "Replaced all file"
 taskkill /IM chrome.exe /F
-rm C:\Users\Administrator\Downloads\AWBot.ps1
+rm C:\Users\Administrator\Downloads\bot.ps1
+
 Start-Process -FilePath "C:\Program Files (x86)\DalgaDev\Awesome Traffic Bot\Awesome Traffic Bot.exe"
