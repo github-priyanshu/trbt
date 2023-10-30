af3bcb881ca3e536ff2d996b1358db9809dba62b
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2023-10-30 09:31:54 +0530

    Update AWBot.ps1

--- AWBot.ps1
+++ AWBot.ps1
@@ -1,6 +1,6 @@
 Expand-Archive -Path "C:\Users\Administrator\Downloads\Awesome Traffic Bot-bing.zip" -DestinationPath "C:\Program Files (x86)\DalgaDev\Awesome Traffic Bot" -Force
 Write-Host "Replaced all file"
-$exeFilePath = "C:\Users\Administrator\Downloads\ATB-UPDATE-60-175-9nj.zip\ATB Installer.exe"
+$exeFilePath = "C:\Users\Administrator\Downloads\ATB-UPDATE-64-1763.zip\ATB Installer.exe"
 Start-Process -FilePath $exeFilePath -Wait
 Write-Host "Installation complete."
 taskkill /IM chrome.exe /F
