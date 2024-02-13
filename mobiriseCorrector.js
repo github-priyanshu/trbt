adc226e4f632bad737d20d3fb4d576b58d4f4e76
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2024-02-13 22:15:21 +0530

    Delete mobiriseCorrector.js

--- mobiriseCorrector.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('strong').forEach(val=>{if(val.innerText.includes('Verified Purchase')){console.log('s')
-  val.parentElement.insertAdjacentHTML('beforebegin',`<img src=''>`)
-}})
-
-document.querySelector('section:nth-last-of-type(1)').remove();
-
-document.querySelectorAll('p,h1,span,strong,h2,h3,h4,h5,h6,i,u,b').forEach(val=>{try{if(!val.firstChild.tagName && (!val.textContent.startsWith('\n') || !val.textContent.startsWith(' ')) && val.textContent.includes("__strike__")){val.style.textDecoration="line-through";val.innerText=val.innerText.replace('__strike__',''); console.log(val) }}catch(e){}})
