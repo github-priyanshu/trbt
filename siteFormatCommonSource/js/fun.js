320e5a1c35fc3924bdbd56b50b17136372bc692b
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2024-02-26 18:12:26 +0530

    Add files via upload

---
+++ siteFormatCommonSource/js/fun.js
@@ -0,0 +1,9 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  x.classList.toggle('responsive')
+}
+  function toggleMenu() {
+    var menuToggler = document.getElementById("menu-toggler");
+    menuToggler.classList.toggle("change");
+    console.log(menuToggler.classList)
+  }
