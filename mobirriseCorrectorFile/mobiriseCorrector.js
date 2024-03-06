dc28c7b05def8308204ef686e21b5ce3c8ed4d64
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2024-03-06 17:11:22 +0530

    Update mobiriseCorrector.js

--- mobirriseCorrectorFile/mobiriseCorrector.js
+++ mobirriseCorrectorFile/mobiriseCorrector.js
@@ -2,6 +2,7 @@ const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.queryS
   val.parentElement.insertAdjacentHTML('beforebegin',`<img src='https://trbt.netlify.app/mobirriseCorrectorFile/5stars.png' alt='5 star rating image' title='5 stars'>`)
 }});
 
-document.querySelector('section:nth-last-of-type(1)').remove();
+var allSec=document.querySelectorAll('section');
+allSec[allSec.length-1].remove();
 
 document.querySelectorAll('p,h1,span,strong,h2,h3,h4,h5,h6,i,u,b').forEach(val=>{try{if(!val.firstChild.tagName && (!val.textContent.startsWith('\n') || !val.textContent.startsWith(' ')) && val.textContent.includes("__strike__")){val.style.textDecoration="line-through";val.innerText=val.innerText.replace('__strike__',''); console.log(val) }}catch(e){}});
