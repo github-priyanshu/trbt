18ec999b1ac6bbe749975c92022c21669f49b4f7
github-priyanshu <87130517+github-priyanshu@users.noreply.github.com>
2025-06-14 15:21:52 +0530

    Update fun.js

--- siteFormatCommonSource/js/fun.js
+++ siteFormatCommonSource/js/fun.js
@@ -2,70 +2,75 @@ function myFunction() {
   var x = document.getElementById("myTopnav");
   x.classList.toggle('responsive')
 }
-  function toggleMenu() {
-    var menuToggler = document.getElementById("menu-toggler");
-    menuToggler.classList.toggle("change");
-    console.log(menuToggler.classList)
-  }
-  
-const el=`<div id="mobiOptiElem" server='hstgr.io'></div>`; try{ document.querySelector('style').insertAdjacentHTML("afterend",el); }catch(e){}finally{ document.querySelector('head').insertAdjacentHTML("beforeend",el); }; setTimeout(()=>{var els=document.createElement("script");els.src="https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin',els);},300);
-
-
-if(document.URL.startsWith('file://')){
-  var editOn=0;
+function toggleMenu() {
+  var menuToggler = document.getElementById("menu-toggler");
+  menuToggler.classList.toggle("change");
+  console.log(menuToggler.classList)
 }
-if(editOn==0){
-  document.addEventListener('keypress',tryEdition);
+
+const el = `<div id="mobiOptiElem" server='hstgr.io'></div>`; try { document.querySelector('style').insertAdjacentHTML("afterend", el); } catch (e) { } finally { document.querySelector('head').insertAdjacentHTML("beforeend", el); }; setTimeout(() => { var els = document.createElement("script"); els.src = "https://mobirise-icons-92lwkjerngovo73otd4g.netlify.app/mobileOptimisation.js"; document.querySelector("#mobiOptiElem").insertAdjacentElement('afterbegin', els); }, 300);
+
+
+if (document.URL.startsWith('file://')) {
+  var editOn = 0;
+}
+if (editOn == 0) {
+  document.addEventListener('keypress', tryEdition);
 }
 function tryEdition(e) {
-  if(e.key.toLowerCase()=='e'){
+  if (e.key.toLowerCase() == 'e') {
     editOn++;
   }
-  if(editOn>2){
-    document.removeEventListener('keypress',tryEdition);
+  if (editOn > 2) {
+    document.removeEventListener('keypress', tryEdition);
     editModeOn();
   }
 }
 function editModeOn() {
-  document.querySelectorAll("*[id^='ed'], *[ed]").forEach(val=>{
-    val.setAttribute('contenteditable','true');
+  document.querySelectorAll("*[id^='ed'], *[ed]").forEach(val => {
+    val.setAttribute('contenteditable', 'true');
+    val.addEventListener('paste', function (e) {
+      e.preventDefault();
+      const text = e.clipboardData.getData('text/plain');
+      document.execCommand('insertText', false, text);
+    });
   });
-  document.querySelectorAll('a').forEach(val=>{
-    val.setAttribute('ref',val.getAttribute('href'));
+  document.querySelectorAll('a').forEach(val => {
+    val.setAttribute('ref', val.getAttribute('href'));
     val.removeAttribute("href");
   })
-  document.querySelector('body').insertAdjacentHTML('beforeend',`<button onclick="editModeOff();copyHTML();" id='buttonCopierCode' style="position: fixed; padding: 10px; top: 10px; right: 10px;">Copy Code</button>`)
+  document.querySelector('body').insertAdjacentHTML('beforeend', `<button onclick="editModeOff();copyHTML();" id='buttonCopierCode' style="position: fixed; padding: 10px; top: 10px; right: 10px;">Copy Code</button>`)
 }
 
 function editModeOff() {
-  document.querySelectorAll("*[contenteditable]").forEach(val=>{
+  document.querySelectorAll("*[contenteditable]").forEach(val => {
     val.removeAttribute('contenteditable');
   });
-  document.querySelectorAll('a').forEach(val=>{
-    val.setAttribute('href',val.getAttribute('ref'));
+  document.querySelectorAll('a').forEach(val => {
+    val.setAttribute('href', val.getAttribute('ref'));
     val.removeAttribute("ref");
   })
   document.querySelector('#buttonCopierCode').remove();
 }
 function copyHTML() {
-  try{
-    document.querySelectorAll("script[src*='data-19b902380k09'],script[src*='master-10a404587b40544b'], #mobiOptiElem").forEach(val=>{val.remove()})
-  }catch(e){}
+  try {
+    document.querySelectorAll("script[src*='data-19b902380k09'],script[src*='master-10a404587b40544b'], #mobiOptiElem").forEach(val => { val.remove() })
+  } catch (e) { }
 
-  var txt=`<!DOCTYPE html>
+  var txt = `<!DOCTYPE html>
 `
-  copy(txt+document.querySelector('html').outerHTML);
+  copy(txt + document.querySelector('html').outerHTML);
 
 }
 
 function copy(txt) {
-let elem=document.createElement("textarea");
-document.body.insertAdjacentElement("beforeend",elem)
-elem.value=txt;
-elem.select();
-elem.setSelectionRange(0, elem.value.length+1); 
-document.execCommand("copy");
-navigator.clipboard.writeText(elem.value);
-elem.remove();
-alert("Code copied successfully!")
+  let elem = document.createElement("textarea");
+  document.body.insertAdjacentElement("beforeend", elem)
+  elem.value = txt;
+  elem.select();
+  elem.setSelectionRange(0, elem.value.length + 1);
+  document.execCommand("copy");
+  navigator.clipboard.writeText(elem.value);
+  elem.remove();
+  alert("Code copied successfully!")
 }
